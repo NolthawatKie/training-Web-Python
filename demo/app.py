697706a315bf436ce3e15005d8b2c161cd4ea78a
Nolthawat Kietsaro <nolthawatkong@gmail.com>
2023-03-14 21:40:54 +0700

    update app.py

--- demo/app.py
+++ demo/app.py
@@ -2,27 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index() :
-    data = {"name" : "Johny", "age" : 35, "salary" : 28000}
-    return render_template("index.html", myData = data)
+def index():
+    data = {"name": "Johny", "age": 35, "salary": 28000}
+    return render_template("index.html", myData=data)
+
 
 @app.route('/about')
-def about() :
+def about():
     lists = ["list1", "list2", "list3"]
-    return render_template("about.html", myList = lists)
+    return render_template("about.html", myList=lists)
+
 
 @app.route('/admin')
-def admin() :
+def admin():
     name = "Anonymous"
     age = 25
-    return render_template("admin.html", myName = name, myAge=age)
+    return render_template("admin.html", myName=name, myAge=age)
+
 
 @app.route('/user/<name>/<yob>')
-def profile(name, yob) :
-    age = 2023 - int(yob) 
-    return ("<h1>Hello {} </h1>".format(name) + 
+def profile(name, yob):
+    age = 2023 - int(yob)
+    return ("<h1>Hello {} </h1>".format(name) +
             "<h1>Now You're {} years old</h1>".format(age))
 
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
